79d779fc1f12a4d84b22a569145a525aef94b727
huusu1996@gmail.com <huusu1996@gmail.com>
2020-09-18 15:41:43 +0700

    Exercise 05

--- Modules/Exercise05/Tests/Http/Controllers/Exercise05ControllerTest.php
+++ Modules/Exercise05/Tests/Http/Controllers/Exercise05ControllerTest.php
@@ -3,14 +3,45 @@
 namespace Modules\Exercise05\Tests\Http\Controllers;
 
 use Tests\TestCase;
+use Modules\Exercise05\Http\Controllers\Exercise05Controller;
 
 /**
  * TODO: make real test
  */
 class Exercise05ControllerTest extends TestCase
 {
-    public function test_it_render_index_page()
+    public function test_can_create_order_success()
     {
-        $this->assertTrue(true);
+        $url = action([Exercise05Controller::class, 'index']);
+        $attribute = [
+            'price' => 1499,
+            'option_receive' => 1,
+            'option_coupon' => 1
+        ];
+        $this->get($url)
+            ->assertStatus(200)
+            ->assertViewIs('exercise05::index');
+
+        $this->post($url, $attribute)
+            ->assertStatus(200)
+            ->assertViewIs('exercise05::detail');
     }
+
+    public function test_can_create_order_fail_by_validate()
+    {
+        $url = action([Exercise05Controller::class, 'store']);
+        $attribute = [
+            'price' => 'aaa',
+            'option_receive' => 1,
+            'option_coupon' => 1
+        ];
+        $this->get($url)
+             ->assertStatus(200)
+             ->assertViewIs('exercise05::index');
+
+        $this->postJson($url, $attribute)
+        ->assertStatus(422)
+        ->assertJsonStructure(['message', 'errors' => ['price']]);    
+    }
+
 }
