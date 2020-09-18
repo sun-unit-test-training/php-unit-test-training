70ba498f8d6ba6994c0979daba5678b1242a401e
Phạm Việt <vietpt.tlu@gmail.com>
2020-09-18 16:15:23 +0700

    init exercise 08

---
+++ Modules/Exercise08/Tests/Services/TicketServiceTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Modules\Exercise08\Tests\Services;
+
+use Modules\Exercise08\Services\TicketService;
+use Tests\TestCase;
+use InvalidArgumentException;
+
+class TicketServiceTest extends TestCase
+{
+    /**
+     * @var TicketService
+     */
+    protected $ticketService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->ticketService = new TicketService();
+    }
+
+    public function test_it_throw_exception_when_not_old_enough()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $data = $this->getSampleData();
+        $data['age'] = -1;
+        $result = $this->ticketService->calculatePrice($data);
+
+        $this->assertEquals($result, false);
+    }
+
+    public function test_throw_exception_when_age_over_120_years_old()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $data = $this->getSampleData();
+        $data['age'] = 121;
+        $result = $this->ticketService->calculatePrice($data);
+
+        $this->assertEquals($result, false);
+    }
+
+    public function test_calculate_price_when_age_less_than_13_years_old()
+    {
+        $data = $this->getSampleData();
+        $data['age'] = 10;
+        $result = $this->ticketService->calculatePrice($data);
+
+        $this->assertEquals($result, $this->ticketService::BASE_PRICE*0.5);
+    }
+
+    public function test_calculate_price_when_day_of_week_is_tuesday()
+    {
+        $data = $this->getSampleData();
+        $data['booking_date'] = '2020/09/22';
+        $result = $this->ticketService->calculatePrice($data);
+
+        $this->assertEquals($result, $this->ticketService::PRICE_IN_TUESDAY);
+    }
+
+    public function test_calculate_price_when_age_from_13_to_64_not_tuesday_friday()
+    {
+        $data = $this->getSampleData();
+        $data['age'] = 18;
+        $data['booking_date'] = '2020/09/24';
+        $result = $this->ticketService->calculatePrice($data);
+
+        $this->assertEquals($result, $this->ticketService::BASE_PRICE);
+    }
+
+    public function test_calculate_price_when_age_from_13_to_64_is_female_on_friday()
+    {
+        $data = $this->getSampleData();
+        $data['age'] = 18;
+        $data['gender'] = 'female';
+        $data['booking_date'] = '2020/09/25';
+        $result = $this->ticketService->calculatePrice($data);
+
+        $this->assertEquals($result, $this->ticketService::PRICE_FEMALE_FRIDAY);
+    }
+
+    public function test_calculate_price_when_age_from_13_to_64_is_male_on_friday()
+    {
+        $data = $this->getSampleData();
+        $data['age'] = 18;
+        $data['gender'] = 'male';
+        $data['booking_date'] = '2020/09/25';
+        $result = $this->ticketService->calculatePrice($data);
+
+        $this->assertEquals($result, $this->ticketService::BASE_PRICE);
+    }
+
+    public function test_calculate_price_when_age_over_65_not_tuesday_friday()
+    {
+        $data = $this->getSampleData();
+        $data['age'] = 69;
+        $data['gender'] = 'male';
+        $data['booking_date'] = '2020/09/27';
+        $result = $this->ticketService->calculatePrice($data);
+
+        $this->assertEquals($result, $this->ticketService::PRICE_OVER_65);
+    }
+
+    public function test_calculate_price_when_age_over_65_is_female_on_friday()
+    {
+        $data = $this->getSampleData();
+        $data['age'] = 69;
+        $data['gender'] = 'female';
+        $data['booking_date'] = '2020/09/25';
+        $result = $this->ticketService->calculatePrice($data);
+
+        $this->assertNotEquals($result, $this->ticketService::PRICE_OVER_65);
+        $this->assertEquals($result, $this->ticketService::PRICE_FEMALE_FRIDAY);
+    }
+
+    private function getSampleData()
+    {
+        return [
+            'age' => 15,
+            'booking_date' => '2020/09/22',
+            'gender' => 'female',
+            'name' => 'Test'
+        ];
+    }
+}
