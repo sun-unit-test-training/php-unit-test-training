520dc316ba9551ee5fd7a427ef81cb700b2964a0
dungda-0794 <dungda-0794>
2020-09-20 01:06:13 +0700

    refs #implements code exercise10

---
+++ Modules/Exercise10/Contracts/Services/PrepaidInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Modules\Exercise10\Contracts\Services;
+
+interface PrepaidInterface
+{
+}
---
+++ Modules/Exercise10/Services/PrepaidCardService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Modules\Exercise10\Services;
+
+use Modules\Exercise10\Contracts\Services\PrepaidInterface;
+
+class PrepaidCardService implements PrepaidInterface
+{
+}
