6a9ec52e9c466c04bd35e8b2df009466b29bca37
Tran Thi Hai Ha <tran.thi.hai.ha@sun-asterisk.com>
2020-09-22 09:11:54 +0700

    exercise02: coding + testing

--- Modules/Exercise02/Tests/Http/Controllers/Exercise02ControllerTest.php
+++ Modules/Exercise02/Tests/Http/Controllers/Exercise02ControllerTest.php
@@ -3,14 +3,65 @@
 namespace Modules\Exercise02\Tests\Http\Controllers;
 
 use Tests\TestCase;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Modules\Exercise02\Http\Controllers\Exercise02Controller;
+use Modules\Exercise02\Http\Requests\ATMRequest;
+use Modules\Exercise02\Models\ATM;
+use Modules\Exercise02\Repositories\ATMRepository;
+use Modules\Exercise02\Services\ATMService;
+use Tests\SetupDatabaseTrait;
 
 /**
  * TODO: make real test
  */
 class Exercise02ControllerTest extends TestCase
 {
-    public function test_it_render_index_page()
+    use SetupDatabaseTrait;
+
+    /**
+     * @var Exercise02Controller
+     */
+    protected $controller;
+
+    /**
+     * @var ATMService
+     */
+    protected $atmService;
+
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setup();
+        $this->atmService = new ATMService(new ATMRepository(new ATM()));
+        $this->controller = new Exercise02Controller($this->atmService);
+    }
+
+    public function test_index_return_view()
+    {
+        $response = $this->controller->index();
+
+        $this->assertInstanceOf(View::class, $response);
+        $this->assertEquals('exercise02::index', $response->getName());
+        $this->assertEquals([
+            'normalFee' => $this->atmService::NORMAL_FEE,
+            'noFee' => $this->atmService::NO_FEE,
+            'timePeriod1' => $this->atmService::TIME_PERIOD_1,
+            'timePeriod2' => $this->atmService::TIME_PERIOD_2,
+            'timePeriod3' => $this->atmService::TIME_PERIOD_3,
+        ], $response->getData());
+    }
+
+    public function test_function_caculate()
+    {
+        $request = \Mockery::mock(ATMRequest::class);
+        $card = factory(ATM::class)->state('is_vip')->create()->fresh();
+        $request->shouldReceive('validated')->andReturn([
+            'card_id' => $card->card_id,
+        ]);
+        $correctAnswer = ['fee' => 0];
+        $response = $this->controller->takeATMFee($request);
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals($correctAnswer,  $response->getSession()->all()['caculate']);
     }
 }
---
+++ Modules/Exercise02/Tests/Repositories/ATMRepositoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Modules\Exercise02\Tests\Repositories;
+
+use Tests\TestCase;
+use Modules\Exercise02\Models\ATM;
+use Modules\Exercise02\Repositories\ATMRepository;
+use Tests\SetupDatabaseTrait;
+
+class ATMRepositoryTest extends TestCase
+{
+    use SetupDatabaseTrait;
+
+    /**
+     * @var ATMRepository
+     */
+    protected $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = new ATMRepository(new ATM());
+    }
+
+    public function test_it_can_find()
+    {
+        $card = factory(ATM::class)->create()->fresh();
+
+        $this->assertEquals($card, $this->repository->find($card->card_id));
+    }
+
+    public function test_it_cannot_find_cart_does_not_exist()
+    {
+        $this->assertNull($this->repository->find(-1));
+    }
+}
