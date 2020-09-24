9666a64ca650e75c74cc5c863361919ea66c960d
Doanh Pham <pham.van.doanh@sun-asterisk.vn>
2020-09-24 16:59:29 +0700

    View + Unittest for Exercise 09

--- Modules/Exercise09/Tests/Http/Controllers/Exercise09ControllerTest.php
+++ Modules/Exercise09/Tests/Http/Controllers/Exercise09ControllerTest.php
@@ -2,15 +2,38 @@
 
 namespace Modules\Exercise09\Tests\Http\Controllers;
 
+use Mockery;
+use Modules\Exercise09\Http\Controllers\Exercise09Controller;
+use Modules\Exercise09\Services\CombatService;
 use Tests\TestCase;
 
-/**
- * TODO: make real test
- */
 class Exercise09ControllerTest extends TestCase
 {
+    protected $combatService;
+
+    public function setUp(): void
+    {
+        $this->afterApplicationCreated(function () {
+            $this->combatService = Mockery::mock($this->app->make(CombatService::class));
+        });
+
+        parent::setUp();
+    }
+
     public function test_it_render_index_page()
     {
-        $this->assertTrue(true);
+        $controller = new Exercise09Controller($this->combatService);
+        $view = $controller->index();
+
+        $this->assertEquals('exercise09::index', $view->getName());
+    }
+
+    public function test_it_return_back_after_submit()
+    {
+        $this->withoutMiddleware();
+        $response = $this->post('/exercise09', []);
+        $response->assertStatus(302);
+        $response->assertSessionHas('status');
+        $response->assertRedirect(session()->previousUrl());
     }
 }
