efc974f2d9654cebc572cd7863807e9f3d003fdc
Tuan Pham <pham.thanh.tuan@sun-asterisk.com>
2021-03-04 03:17:13 +0700

    Update tests exercise01

--- Modules/Exercise01/Tests/Feature/Models/VoucherTest.php
+++ Modules/Exercise01/Tests/Feature/Models/VoucherTest.php
@@ -28,23 +28,4 @@ class VoucherTest extends TestCase
         $this->assertEquals($inputs['code'], $voucher->code);
         $this->assertEquals($inputs['is_active'], $voucher->is_active);
     }
-
-    /**
-     * This test does not count coverage for model Voucher,
-     * because we are test for class property `casts`, not method?
-     *
-     * But it is added to ensure we initialize property correctly
-     */
-    public function test_field_is_active_is_cast_to_boolean()
-    {
-        $inputs = [
-            'code' => 'voucher-code',
-            'is_active' => 1,
-        ];
-
-        // No need to persist to db to test cast
-        $voucher = (new Voucher())->fill($inputs);
-
-        $this->assertSame(true, $voucher->is_active);
-    }
 }
