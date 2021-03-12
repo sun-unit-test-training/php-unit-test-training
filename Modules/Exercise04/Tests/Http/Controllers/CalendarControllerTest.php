67c27124cbdbcf7cf12e399f8843c16c5f8d3cd1
lebaongan <le.thi.bao.ngan@sun-asterisk.com>
2021-03-12 18:59:15 +0700

    Refactor Exercise 04

--- Modules/Exercise04/Tests/Http/Controllers/CalendarControllerTest.php
+++ Modules/Exercise04/Tests/Http/Controllers/CalendarControllerTest.php
@@ -3,6 +3,7 @@
 namespace Modules\Exercise04\Tests\Http\Controllers;
 
 use Modules\Exercise04\Http\Controllers\CalendarController;
+use Modules\Exercise04\Services\CalendarService;
 use Tests\TestCase;
 use Tests\SetupDatabaseTrait;
 
@@ -10,9 +11,21 @@ class CalendarControllerTest extends TestCase
 {
     use SetupDatabaseTrait;
 
+    protected $calendarServiceMock;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->calendarServiceMock = $this->mock(CalendarService::class);
+    }
+
     function test_it_show_calendar_view()
     {
         $url = action([CalendarController::class, 'index']);
+
+        $dummyClass = CalendarService::COLOR_BLACK;
+        $this->calendarServiceMock->shouldReceive('getDateClass')->andReturn($dummyClass);
 
         $response = $this->get($url);
 
